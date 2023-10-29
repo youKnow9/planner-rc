f7945a67b8490582f4e7e5a231f52c782fb2b74c
youKnow9 <l.pelevina.che@gmail.com>
2023-10-29 17:49:34 +0300

    add fonts

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Calendar from './Calendar/Calendar.jsx'
 import '../src/Calendar/Calendar.scss'
-// import 'react-big-calendar/lib/css/react-big-calendar.css';
-// import './main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
